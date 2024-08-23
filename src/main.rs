034d5dc29bfe90eb3ee843b2f7e4e0f46d18de7b
Gleicon Moraes <gleicon@gmail.com>
2024-08-22 23:23:41 -0300

    globy mcglobersonface

--- src/main.rs
+++ src/main.rs
@@ -28,38 +28,6 @@ fn main() -> tantivy::Result<()> {
 
     let _ = index_all_pdfs(title, body, &mut index_writer);
 
-    let mut old_man_doc = TantivyDocument::default();
-    old_man_doc.add_text(title, "The Old Man and the Sea");
-    old_man_doc.add_text(
-        body,
-        "He was an old man who fished alone in a skiff in the Gulf Stream and he had gone \
-         eighty-four days now without taking a fish.",
-    );
-    index_writer.add_document(old_man_doc)?;
-
-    index_writer.add_document(doc!(
-    title => "Of Mice and Men",
-    body => "A few miles south of Soledad, the Salinas River drops in close to the hillside \
-            bank and runs deep and green. The water is warm too, for it has slipped twinkling \
-            over the yellow sands in the sunlight before reaching the narrow pool. On one \
-            side of the river the golden foothill slopes curve up to the strong and rocky \
-            Gabilan Mountains, but on the valley side the water is lined with trees—willows \
-            fresh and green with every spring, carrying in their lower leaf junctures the \
-            debris of the winter’s flooding; and sycamores with mottled, white, recumbent \
-            limbs and branches that arch over the pool"
-    ))?;
-
-    index_writer.add_document(doc!(
-    title => "Frankenstein",
-    title => "The Modern Prometheus",
-    body => "You will rejoice to hear that no disaster has accompanied the commencement of an \
-             enterprise which you have regarded with such evil forebodings.  I arrived here \
-             yesterday, and my first task is to assure my dear sister of my welfare and \
-             increasing confidence in the success of my undertaking."
-    ))?;
-
-    index_writer.commit()?;
-
     let reader = index
         .reader_builder()
         .reload_policy(ReloadPolicy::OnCommitWithDelay)
@@ -69,7 +37,7 @@ fn main() -> tantivy::Result<()> {
 
     let query_parser = QueryParser::for_index(&index, vec![title, body]);
 
-    let query = query_parser.parse_query("sea whale")?;
+    let query = query_parser.parse_query("linux")?;
 
     let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
     for (_score, doc_address) in top_docs {
@@ -103,12 +71,13 @@ fn index_all_pdfs(
         ..Default::default()
     };
 
-    for entry in glob_with("/Downloads/*.pdf", options)? {
+    for entry in glob_with("/Users/gleicon/Downloads/*.pdf", options)? {
         match entry {
             Ok(pb) => {
                 println!("parsing {}", pb.clone().display());
                 match parse_pdf(pb.clone()) {
                     Ok(out) => {
+                        //println!("out: {:?}", out);
                         let mut le_doc = TantivyDocument::default();
                         le_doc.add_text(title, pb.clone().display());
 
@@ -116,18 +85,19 @@ fn index_all_pdfs(
                         let _ = index_writer.add_document(le_doc);
 
                         let _ = index_writer.commit();
-                        // return Ok(());
-
-                        //println!("{:?}", out[0..10].to_string())
+                        return Ok("Parsed".into());
                     }
                     Err(e) => {
-                        println!("Error {:?}", e);
+                        println!("Error Parsing {:?}", e);
                         return Err(e);
                     }
                 }
                 // parse
             }
-            Err(e) => (),
+            Err(e) => {
+                println!("Error {}", e);
+                ()
+            }
         }
     }
 
@@ -150,11 +120,8 @@ fn parse_pdf(file: PathBuf) -> Result<Vec<String>> {
             Ok(texts)
         }
         Err(e) => {
-            eprintln!("Error: {}", e);
-            //return Err(io::Error::new(io::ErrorKind::InvalidData, err.into()));
+            println!("Error: {}", e);
             return Err("erro".into());
-            //Err(err.into());
-            //                return Err(*Box::from(io::Error::new(err.to_string()))); //Err(err.into());
         }
     }
 }
